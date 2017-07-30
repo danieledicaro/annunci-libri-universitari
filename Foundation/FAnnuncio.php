671286f9ac299f471a5236ae0d9830dacead6dbd
Enrico Sciulli <loool43@hotmail.it>
2017-07-30 18:05:42 +0200

    Aggiornamento

--- Foundation/FAnnuncio.php
+++ Foundation/FAnnuncio.php
@@ -8,10 +8,11 @@
 
 class FAnnuncio extends Fdb {
     public function __construct() {
+        parent::__construct();
         $this->_table='Annuncio';
         $this->_key= 'id_annuncio';
         $this->_return_class='EAnnuncio';
-        parent::__construct();
+        $this->_auto_increment=true;
         //USingleton::getInstance('Fdb');
     }
 
