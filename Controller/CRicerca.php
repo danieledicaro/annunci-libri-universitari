b7b82e0f2d78808f19fa381db775f7521a72bafa
Forxing <loool43@hotmail.it>
2017-08-08 15:29:21 +0200

    Primi abbozzi di Control e View

---
+++ Controller/CRicerca.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enrico
+ * Date: 07/08/17
+ * Time: 16.48
+ */
+
+class CRicerca {
+    /**
+     * @var int
+     */
+    private $_annunci_per_pagina=10;
+
+    /**
+     * Seleziona sul database i libri con id più alto e li mostra nella pagina principale
+     *
+     * @return string contenuto del template processato
+     */
+    /*public function ultimiArrivi() {
+        $view = USingleton::getInstance('VRicerca');
+        $this->_annunci_per_pagina=4;
+        $FLibro=new FLibro();
+        $limit=$view->getPage()*$this->_annunci_per_pagina.','.$this->_annunci_per_pagina;
+        $num_risultati=count($FLibro->search());
+        $pagine = ceil($num_risultati/$this->_annunci_per_pagina);
+        $risultato=$FLibro->search(array(), '`ISBN` DESC', $limit);
+        if ($risultato!=false) {
+            $array_risultato=array();
+            foreach ($risultato as $item) {
+                $tmpLibro=$FLibro->load($item->ISBN);
+                $array_risultato[]=array_merge(get_object_vars($tmpLibro),array('media_voti'=>$tmpLibro->getMediaVoti()));
+            }
+        }
+        $view->impostaDati('pagine',$pagine);
+        $view->impostaDati('task','ultimi_arrivi');
+        $view->impostaDati('dati',$array_risultato);
+        return $view->processaTemplate();
+    }*/
+
+    /**
+     * Seleziona sul database i libri per mostrarli all'utente e li filtra
+     * in base alle variabili passate
+     * es categorie o stringhe di ricerca
+     *
+     * @return string
+     */
+    public function lista(){
+        $view = USingleton::getInstance('VRicerca');
+        $FCatalogo = new FCatalogo();
+        $parametri = array($view->getParola());
+        $limit = $view->getPage()*$this->_annunci_per_pagina.','.$this->_annunci_per_pagina;
+        $num_risultati=count($FCatalogo->search(array($parametri, '', '')));
+        $pagine = ceil($num_risultati/$this->_annunci_per_pagina);
+        $foo = array ($parametri, 'data', $limit);
+        $risultato = $FCatalogo->search($foo)->getCatalogo(); //array di EAnnunci
+        $view->impostaDati('pagine',$pagine);
+        $view->impostaDati('task','lista');
+        $view->impostaDati('parametri','keyword='.$parametri);
+        $view->impostaDati('dati',$risultato);
+        return $view->processaTemplate();
+    }
+
+    /**
+     * Mostra i dettagli di un libro, con la descrizione completa, i commenti e il form per l'invio di commenti, solo per utenti registrati
+     *
+     * @return string
+     */
+    public function dettagli() {
+        $view = USingleton::getInstance('VRicerca');
+        $id_annuncio = $view->getIdAnnuncio();
+        $FAnnuncio = new FAnnuncio();
+        $annuncio = $FAnnuncio->load($id_annuncio);
+        $dati = $annuncio->getObjectVars();
+        $view->impostaDati('dati',$dati);
+        return $view->processaTemplate();
+    }
+
+    /**
+     * Inserisce un commento nel database collegandolo al relativo libro
+     *
+     * @return string
+     */
+    /*public function inserisciCommento() {
+        $session=USingleton::getInstance('USession');
+        $username=$session->leggi_valore('username');
+        if ($username!=false) {
+            $view = USingleton::getInstance('VRicerca');
+            $ECommento = new ECommento();
+            $ECommento->libroISBN=$view->getIdLibro();
+            $ECommento->voto=$view->getVoto();
+            $ECommento->testo=$view->getCommento();
+            $FCommento=new FCommento();
+            $FCommento->store($ECommento);
+            return $this->dettagli();
+        }
+    }*/
+
+    /**
+     * Smista le richieste ai vari metodi
+     *
+     * @return mixed
+     */
+    public function smista() {
+        $view=USingleton::getInstance('VRegistrazione');
+        switch ($view->getTask()) {
+            case 'dettagli':
+                return $this->dettagli();
+            case 'Contatta venditore':
+                $CMessaggio = USingleton::getInstance('CMessaggio');
+                return $CMessaggio->smista();
+            case 'lista':
+                return $this->lista();
+            case 'Cerca':
+                return $this->lista();
+        }
+    }
+
+}
---
+++ View/VRicerca.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enrico
+ * Date: 08/08/17
+ * Time: 12.57
+ */
+
+class VRicerca {
+
+    /**
+     * @var string _layout
+     */
+    private $_layout='default';
+
+    /**
+     * restituisce il numero della pagina (utilizzato nella visualizzazione dei libri) passato per GET o POST
+     * @return int
+     */
+    public function getPage() {
+        if (isset($_REQUEST['page'])) {
+            return $_REQUEST['page'];
+        } else
+            return 0;
+    }
+
+    /**
+     * Processa il layout scelto nella variabile _layout
+     *
+     * @return string
+     */
+    public function processaTemplate() {
+        return $this->fetch('ricerca_'.$this->_layout.'.tpl');
+    }
+
+    /**
+     * Imposta i dati nel template identificati da una chiave ed il relativo valore
+     *
+     * @param string $key
+     * @param mixed $valore
+     */
+    public function impostaDati($key,$valore) {
+        $this->assign($key,$valore);
+    }
+
+    /**
+     * Ritorna l'id del libro passato tramite GET o POST
+     *
+     * @return mixed
+     */
+    public function getIdAnnuncio() {
+        if (isset($_REQUEST['id_annuncio'])) {
+            return $_REQUEST['id_annuncio'];
+        } else
+            return false;
+    }
+
+    /**
+     * @param string $layout
+     */
+    /*public function setLayout($layout) {
+        $this->_layout=$layout;
+    }*/
+
+    /**
+     * restituisce il voto passato tramite GET o POST
+     *
+     * @return mixed
+     */
+    /*public function getVoto() {
+        if (isset($_REQUEST['voto'])) {
+            return $_REQUEST['voto'];
+        } else
+            return false;
+    }*/
+
+    /**
+     * Restituisce il commento
+     *
+     * @return mixed
+     */
+    /*public function getCommento() {
+        if (isset($_REQUEST['commento'])) {
+            return $_REQUEST['commento'];
+        } else
+            return false;
+    }*/
+
+    /**
+     * Restituisce categoria
+     *
+     * @return mixed
+     */
+    /*public function getCategoria() {
+        if (isset($_REQUEST['categoria']))
+            return $_REQUEST['categoria'];
+        else
+            return false;
+    }*/
+
+    /**
+     * restituisce la stringa di ricerca
+     *
+     * @return mixed
+     */
+    public function getParola() {
+        if (isset($_REQUEST['stringa']))
+            return $_REQUEST['stringa'];
+        else
+            return false;
+    }
+
+}
