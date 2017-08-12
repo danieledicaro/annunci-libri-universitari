e9e8a385e3297dd5abf544cd9b0cf26e95612e0c
Forxing <loool43@hotmail.it>
2017-08-12 15:30:08 +0200

    Completamento delle classi, creazione index e autoload (copy/paste da bookstore), mancano i template

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,12 +1,21 @@
 <?php
 global $config;
 
-$config['debug']=true;
+$config['debug']=false;
 $config['db']['type'] = 'mysql';
 $config['db']['user'] = 'root';
 $config['db']['password'] = '';
 $config['db']['host'] = '127.0.0.1';
 $config['db']['dbname'] = 'Unibookstore';
+
+$config['smarty']['template_dir'] =
+    '/opt/lampp/htdocs/unibookstore/templates/main/template/';
+$config['smarty']['compile_dir'] =
+    '/opt/lampp/htdocs/unibookstore/templates/main/templates_c/';
+$config['smarty']['config_dir'] =
+    '/opt/lampp/htdocs/unibookstore/templates/main/configs/';
+$config['smarty']['cache_dir'] =
+    '/opt/lampp/htdocs/unibookstore/templates/main/cache/';
 
 
 function debug($var){
--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
-//dobbiamo istanziare le nostre classe, abbiamo scelto FUtente
-//per l'inclusione file, slide di Cicerone 09_PHP-OOP
-function __autoload($class_name)
-{
-    if (strpos($class_name, "USingleton")===false) {             //se il nome della classe sta in foundation lo cerco lì, altrimenti cerco in foundation Utility
-        include_once "Foundation/{$class_name}.php";
-}
-else {
-    include_once "Foundation/Utility/{$class_name}.php";
-}
-}
-    include_once "Foundation/FUtente.php";
-    include_once "Foundation/Fdb.php";
-    $fdb = new Fdb();
-    $utente = new FUtente();
+/*
+ * Effettuo il redirect a main/index.php
+ */
+require_once 'includes/autoload.inc.php';
+require_once 'includes/config.inc.php';
+
+$CHome=USingleton::getInstance('CHome');
+$CHome->impostaPagina();
+
 ?>
