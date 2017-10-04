b68d4c89fb42a0320b43030ed5fe869ce5568014
Daniele Di Caro <iphoneddc90@gmail.com>
2017-10-04 19:12:03 +0200

    fix query

--- Foundation/FCatalogo.php
+++ Foundation/FCatalogo.php
@@ -33,9 +33,9 @@ class FCatalogo extends Fdb {
                 " `Annuncio`.`corso`, `Annuncio`.`citta_consegna`, `Annuncio`.`se_spedisce`, `Annuncio`.`descrizione`,".
                 " `Annuncio`.`condizione`, `Annuncio`.`prezzo` ".
                 "FROM `Annuncio`, `Libro`, `CasaEditrice`, `Autore`, `AutoreLibro`, `Corso`, `Professore`, `Universita` ".
-                "WHERE `Annuncio`.descrizione LIKE '%".$b[0]."%' OR `Libro`.titolo LIKE '%".$b[0]."%' OR `Libro`.isbn LIKE '%".$b[0].
+                "WHERE (`Annuncio`.descrizione LIKE '%".$b[0]."%' OR `Libro`.titolo LIKE '%".$b[0]."%' OR `Libro`.isbn LIKE '%".$b[0].
                 "%' OR `CasaEditrice`.nome LIKE "."'%".$b[0]."%' OR `Autore`.nome LIKE '%".$b[0]."%' OR `Universita`.nome LIKE '%".$b[0].
-                "%' OR `Professore`.nome LIKE '%".$b[0]."%' OR `Corso`.nome LIKE '%".$b[0]."%' AND `Libro`.isbn = `Annuncio`.libro ";
+                "%' OR `Professore`.nome LIKE '%".$b[0]."%' OR `Corso`.nome LIKE '%".$b[0]."%') AND `Libro`.isbn = `Annuncio`.libro ";
         }
         else {
             if ( $b[0]!=''){
