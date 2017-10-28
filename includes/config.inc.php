cb82d041534302ae058065beb401054b70893498
Ilariabru <ilariabruni93@hotmail.it>
2017-10-28 10:41:33 +0200

    unico cambiamento, riaggiunta del path personale

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -8,11 +8,11 @@ $config['db']['password'] = '';
 $config['db']['host'] = 'localhost';
 $config['db']['dbname'] = 'my_unibookstore1';
 
-
-$config['smarty']['template_dir'] = '/membri/unibookstore1/Templates/main/template';
-$config['smarty']['compile_dir'] = '/membri/unibookstore1/Templates/main/templates_c/';
-$config['smarty']['config_dir'] = '/membri/unibookstore1/Templates/main/configs/';
-$config['smarty']['cache_dir'] = '/membri/unibookstore1/Templates/main/cache/';
+$pathPersonale = 'C:\xampp\htdocs\annunci-libri-universitari';
+$config['smarty']['template_dir'] = $pathPersonale;
+$config['smarty']['compile_dir'] = $pathPersonale;
+$config['smarty']['config_dir'] = $pathPersonale;
+$config['smarty']['cache_dir'] = $pathPersonale;
 
 
 function debug($var){
